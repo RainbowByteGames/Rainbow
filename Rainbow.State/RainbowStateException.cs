f7de8476abd165be203b663ef5f3e76f6bf615f4
Hazdryx <hazdryx@gmail.com>
2022-09-03 08:38:25 -0500

    Cleaned up namespaces

--- Rainbow.State/RainbowStateException.cs
+++ Rainbow.State/RainbowStateException.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Rainbow.State
+﻿namespace Rainbow.State
 {
     /// <summary>
     ///     An exception for state errors.
