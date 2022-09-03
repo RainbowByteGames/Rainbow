ef2c31c8328ca72d75ae29807716811b40b1c72b
Hazdryx <hazdryx@gmail.com>
2022-09-03 09:04:12 -0500

    Added `Rainbow.Graphics` project.

---
+++ Rainbow.Graphics/SpriteBatchExt.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Rainbow.Math;
+
+namespace Rainbow.Graphics
+{
+    /// <summary>
+    ///     Some extension methods for the SpriteBatch.
+    /// </summary>
+    public static class SpriteBatchExt
+    {
+        /// <summary>
+        ///     Draws a texture to the sprite batch.
+        /// </summary>
+        /// <param name="batch"></param>
+        /// <param name="texture"></param>
+        /// <param name="transform"></param>
+        /// <param name="sourceRectangle"></param>
+        /// <param name="color"></param>
+        /// <param name="normalOrigin"></param>
+        /// <param name="effects"></param>
+        /// <param name="layer"></param>
+        public static void Draw(this SpriteBatch batch, Texture2D texture, Transform2D transform, Rectangle sourceRectangle, Color color, Vector2 normalOrigin, SpriteEffects effects, int layer)
+        {
+            batch.Draw(texture, transform.Position, sourceRectangle, color, transform.Rotation, new Vector2
+            {
+                X = normalOrigin.X * sourceRectangle.Width,
+                Y = normalOrigin.Y * sourceRectangle.Height
+            }, transform.Scale, effects, layer);
+        }
+        /// <summary>
+        ///     Draws a sprite to the sprite batch.
+        /// </summary>
+        /// <param name="batch"></param>
+        /// <param name="sprite"></param>
+        /// <param name="transform"></param>
+        public static void Draw(this SpriteBatch batch, Sprite sprite, Transform2D transform)
+        {
+            batch.Draw(sprite.Texture, transform, sprite.SourceRectangle, sprite.Color, sprite.Origin, sprite.Effects, sprite.Layer);
+        }
+    }
+}
