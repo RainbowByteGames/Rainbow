1e29af74e329442da732c94625f877aa9347f3e1
Hazdryx <hazdryx@gmail.com>
2022-09-18 06:47:49 -0500

    Implemented the foundation for the `RainbowState`

---
+++ Rainbow/RainbowState.cs
@@ -0,0 +1,14 @@
+﻿using HappiiDreamer.Rainbow.State;
+
+namespace HappiiDreamer.Rainbow
+{
+    public abstract class RainbowState : GameState
+    {
+        public RainbowGame Rainbow { get; }
+
+        public RainbowState(StateManager states, int id) : base(states, id)
+        {
+            Rainbow = (RainbowGame) Game;
+        }
+    }
+}
