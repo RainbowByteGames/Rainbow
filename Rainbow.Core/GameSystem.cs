fd832d7426e9a4082dc50929bce89dcc639b3015
Hazdryx <hazdryx@gmail.com>
2022-09-18 05:50:46 -0500

    Fixed `GameSystem` not being an `IGameSystem`.

--- Rainbow.Core/GameSystem.cs
+++ Rainbow.Core/GameSystem.cs
@@ -4,12 +4,22 @@ using Microsoft.Xna.Framework;
 
 namespace HappiiDreamer.Rainbow
 {
-    public abstract class GameSystem : GameObject
+    public abstract class GameSystem : IGameSystem
     {
-        public GameSystem(Game game) : base(game) { }
+        public Game Game { get; }
+        public abstract bool IsUpdatable { get; }
+        public abstract bool IsDrawable { get; }
+
+        public GameSystem(Game game)
+        {
+            Game = game;
+        }
 
         public virtual bool BeginDraw() { return true; }
+        public virtual void Draw(GameTime gameTime) { }
         public virtual void EndDraw() { }
+
+        public virtual void Update(GameTime gameTime) { }
 
         public virtual void OnActivated(object? sender, EventArgs e) { }
         public virtual void OnDeactivated(object? sender, EventArgs e) { }
